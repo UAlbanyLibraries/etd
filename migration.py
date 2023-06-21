f8e5b66d82698651001a0ef274e616ea872c2dba
kmules1 <50674690+kmules1@users.noreply.github.com>
2023-06-21 10:22:23 -0400

    debugged to initial stages of output generation

--- migration.py
+++ migration.py
@@ -4,17 +4,17 @@ import os
 import sys
 import getopt
 import xlwt
-from etd import ETD
+from etd import etd
 from lxml import etree
 import envconfig
+
+working_directory = '\\\\Lincoln\\Library\\ETDs'
+storage_directory = '\\\\Lincoln\\Masters\\ETD-storage'
 
 def main(argv):
     print('Initiating...');
     
     #setup
-    working_directory = '\\\\Lincoln\\Library\\ETDs'
-    storage_directory = '\\\\Lincoln\\Masters\\ETD-storage'
-    
     input_file = ''
     output_file = ''
     
@@ -88,7 +88,7 @@ def main(argv):
             output_ws.write(0,column,header['output']);
         
         # read input file
-        with open(input_file, newline='') as input_csv:
+        with open(input_file, newline='', encoding="utf8") as input_csv:
             input_reader = csv.reader(input_csv)
             input_headers = next(input_reader)
             
@@ -98,63 +98,101 @@ def main(argv):
                 xml_id = row_array[2]
                 year   = row_array[3]
                 
+                print(' '.join([mms_id, etd_id, year]))
+                
                 # retrieve records and flatten
                 bib_record = get_bib(mms_id)
                 etd_record = get_etd(etd_id, xml_id, year)
-                data = flatten_data(headers,bib_record,etd_record)
+                
+                data = flatten_data(headers,bib_record,etd_record, mms_id,etd_id,xml_id,year)
                 
                 # write entry into output_ws
                 for column,header in enumerate(headers):
+                    #print('Column ' + str(column) + ':' + header['output'])
                     output_ws.write(row_count+1,column,data[header['output']])
         #save output file
         output_wb.save(output_file)
 
 #combines data from bib record and etd record to produce a flat object that contains the necessary field values
-def flatten_data(headers,bib_record,etd_record):
-    data = object()
+def flatten_data(headers,bib_record,etd_record, mms_id,etd_id,xml_id,year):
+    data = {}
     
-    bib_anies = etree.fromstring(bib_record['anies'])
-    etd_xml = etree.fromstring(etd_record.xml_file)
-    
-    for header in headers:
-        header_input = header['input']
-        
-        
-        match header_input:
-            case 'title' | 'mms_id' | 'date_of_publication': #taken from bib data json
-                data[header['output']] = bib_record[header['input']]
-            case '050' | '060' | '300': #taken from bib data xml
-                datafield = root.xpath("//datafield[@tag=" + header['input'] +"]")
-                data[header['output']] = "".join(datafield.itertext())
-            case 'abstract' | 'keyword' | 'inst_name': #taken from etd's ..._DATA.xml
-                data[header['output']] = "".join(etd_xml.find('DISS_' + header['input']).itertext())
-            case 'First-Name' | 'Middle-Name' | 'Last-Name' | 'ProQuest-Email' : #taken from etd metadata in bag-info.txt
-                data[header['output']] = etd.bag.info[header['input']]
-            case 'advisor': #taken from etd ..._DATA.xml, specific to advisor group
-                decription = etd_xml.find('DISS_description')
-                advisors = decription.findall('DISS_advisor')
-                
-                data['advisor1'] = advisors[0].find('DISS_fname').text + ' ' + advisors[0].find('DISS_middle').text + ' ' + advisors[0].find('DISS_surname').text
-                
-                if (len(advisors) > 1):
-                    data['advisor2'] = advisors[1].find('DISS_fname').text + ' ' + advisors[1].find('DISS_middle').text + ' ' + advisors[1].find('DISS_surname').text
+    #this fills the row with an error report if the associated input data is corrupt
+    if etd_record is None or bib_record is None:
+        for i,header in enumerate(headers):
+            if i == 0:
+                if bib_record == None:
+                    data[header['output']] = 'Bib Record Not Retrieved: ' + mms_id
                 else:
-                    data['advisor2'] = ''
-                
-                if (len(advisors) > 2):
-                    data['advisor3'] = advisors[2].find('DISS_fname').text + ' ' + advisors[2].find('DISS_middle').text + ' ' + advisors[2].find('DISS_surname').text
+                    data[header['output']] = 'Bib Record Fine: ' + mms_id
+            elif i == 1:
+                if etd_record == None:
+                    data[header['output']] = 'ETD Record Not Retrieved: ' + etd_id + ' ' + year + ' ' + xml_id
                 else:
-                    data['advisor3'] = ''
-            case 'categorization': #taken from etd ..._DATA.xml, specific to categories group
-                categorization = etd_xml.find('DISS_categorization')
-                categories = categorization.findall('DISS_category')
-                disciplines = ''
-                
-                for category in categories:
-                    disciplines = disciplines.join(category.find('DISS_cat_desc')).text
-                data[header['output']] = disciplines
-            case _: #unassigned elsewhere, leave blank for now
+                    data[header['output']] = 'ETD Record Fine: ' + etd_id + ' ' + year + ' ' + xml_id
+            else:
                 data[header['output']] = ''
+    else:
+        bib_xml = etree.XML(bib_record['anies'][0].encode(encoding="UTF-16"))
+        etd_xml = etree.parse(etd_record.xml_file)
+    
+        for header in headers:
+            header_input = header['input']
+            header_output = header['output']
+            
+            match header_input:
+                case 'title' | 'mms_id' | 'date_of_publication': #taken from bib data json
+                    data[header['output']] = bib_record[header['input']]
+                case '050' | '060' | '300': #taken from bib data xml
+                    datafields = bib_xml.xpath("//datafield[@tag=" + header['input'] +"]")
+                    data[header['output']] = ''
+                    
+                    for i,datafield in enumerate(datafields):
+                        if i > 0:
+                            data[header['output']] += '\n'
+                        subfields = datafield.findall('subfield')
+                        
+                        for j,subfield in enumerate(subfields):
+                            if j > 0:
+                                data[header['output']] += ' '
+                            data[header['output']] += subfield.text
+                case 'abstract' | 'keyword' | 'inst_name': #taken from etd's ..._DATA.xml
+                    found_els = etd_xml.find('DISS_' + header['input'])
+                    if found_els != None:
+                        data[header['output']] = ''.join(found_els.itertext())
+                    else:
+                        data[header['output']] = ''
+                case 'First-Name' | 'Middle-Name' | 'Last-Name' | 'ProQuest-Email' : #taken from etd metadata in bag-info.txt
+                    if header['input'] in etd_record.bag.info:
+                        data[header['output']] = etd_record.bag.info[header['input']]
+                    else:
+                        data[header['output']] = ''
+                case 'advisor': #taken from etd ..._DATA.xml, specific to advisor group
+                    advisors = etd_xml.findall('DISS_advisor')
+                    
+                    data['advisor1'] = ''
+                    data['advisor2'] = ''
+                    data['advisor3'] = ''
+                    
+                    for i,advisor in enumerate(advisors):
+                        advisor_name = [];
+                        if advisor.find('DISS_fname').text:
+                            advisor_name[0] = advisor.find('DISS_fname').text.title()
+                        if advisor.find('DISS_middle').text:
+                            advisor_name[1] = advisor.find('DISS_middle').text.title()
+                        if advisor.find('DISS_surname').text:
+                            advisor_name[1] = advisor.find('DISS_surname').text.title()
+                        
+                        data['advisor' + (i+1)] = ' '.join(advisor_name)
+                case 'categorization': #taken from etd ..._DATA.xml, specific to categories group
+                    categories = etd_xml.findall('DISS_category')
+                    disciplines = ''
+                    
+                    for category in categories:
+                        disciplines = disciplines.join(category.find('DISS_cat_desc')).text
+                    data[header['output']] = disciplines
+                case _: #unassigned elsewhere, leave blank for now
+                    data[header['output']] = ''
     
     return data
     
@@ -162,17 +200,21 @@ def get_bib(mmsid):
     url = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/' + mmsid + '?apikey=' + envconfig.api_key
     headers = {'Accept':'application/json'}
     
-    response = requests.post(url, headers=headers);
+    response = requests.get(url, headers=headers)
+    
     if response.status_code == 200:
         return response.json()
     
     return None
 
 def get_etd(etd_id,xml_id,year):
-    etd = ETD()
-    etd.load(os.path.join(storage_directory, year, etd_id))
+    etd_record = etd()
     
-    return etd
+    if not os.path.isdir(os.path.join(storage_directory, year, etd_id)):
+        return None;
+    else:
+        etd_record.load(os.path.join(storage_directory, year, etd_id))
+        return etd_record
 
 if __name__ == "__main__":
     main(sys.argv[1:])
